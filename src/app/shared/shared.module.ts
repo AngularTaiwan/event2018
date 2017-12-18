f17e56bcf384d64066be12aa08b79224f38fa075
Kevin Yang <chgc_kevin@hotmail.com>
2017-12-18 13:55:55 +0800

    新增議程、票價初版頁面

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,28 @@ import { ContactComponent } from './contact/contact.component';
 import { AgmCoreModule } from '@agm/core';
 import { SpeakersComponent } from './speakers/speakers.component';
 import { AnchorClickDirective } from './anchor-click.directive';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   imports: [CommonModule, AgmCoreModule],
-  declarations: [CountdownComponent, HeaderComponent, ContactComponent, SpeakersComponent, AnchorClickDirective],
-  exports: [CountdownComponent, HeaderComponent, ContactComponent, SpeakersComponent, AnchorClickDirective]
+  declarations: [
+    CountdownComponent,
+    HeaderComponent,
+    ContactComponent,
+    SpeakersComponent,
+    AnchorClickDirective,
+    ScheduleComponent,
+    PriceComponent
+  ],
+  exports: [
+    CountdownComponent,
+    HeaderComponent,
+    ContactComponent,
+    SpeakersComponent,
+    AnchorClickDirective,
+    ScheduleComponent,
+    PriceComponent
+  ]
 })
 export class SharedModule {}
