fa757fd5c9a3dc4206a4c1c27991d19e63d1d49c
Kevin Yang <chgc_kevin@hotmail.com>
2018-01-18 10:56:39 +0800

    調整專案架構
    新增 asp.net core 為底層，加上 rendertron middleware 功能

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {  
+}
---
+++ server/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Spa()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}
