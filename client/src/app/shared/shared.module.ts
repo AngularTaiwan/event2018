8b74f7ebe378ff70be93d79d87fa1bf6907f39d8
林耕瑭 Ben <ben@miniasp.com>
2018-06-04 11:55:36 +0800

    init ticket

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,27 @@
 import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule
+} from '@angular/material';
 
 import { AnchorClickDirective } from './anchor-click.directive';
 import { CountdownComponent } from './countdown/countdown.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-const materialModule = [MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule, MatCardModule];
+const materialModule = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule
+];
+
 @NgModule({
   imports: [CommonModule, AgmCoreModule, materialModule],
   declarations: [
