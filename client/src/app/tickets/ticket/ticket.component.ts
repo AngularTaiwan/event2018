e0bd9c2f27ca1bf235b2686ed97403629406478f
Jeff Wu <crazy.alu@outlook.com>
2018-06-06 23:42:21 +0800

    Add ticket style for ribbon

--- client/src/app/tickets/ticket/ticket.component.ts
+++ client/src/app/tickets/ticket/ticket.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket.component.scss']
 })
 export class TicketComponent implements OnInit {
-
+  sectionTime= '09:00-15:30';
+  sectionName = '入門工作坊';
+  sectionPrice = 6666;
   constructor() { }
 
   ngOnInit() {
