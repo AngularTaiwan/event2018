ab39a61c03b5a0a2b6d534b6c63fe4e87c51fe43
loracy <lora860227@gmail.com>
2018-07-01 02:50:08 +0800

    修改second樣式

--- client/src/app/second/second.component.ts
+++ client/src/app/second/second.component.ts
@@ -15,7 +15,13 @@ export class SecondComponent implements OnInit {
   @HostListener('document:scroll', ['$event']) onScrollEvent($event) {
     const windowHeight = $event.srcElement.documentElement.clientHeight;
     const windowScroll = $event.srcElement.documentElement.scrollTop;
-    if (windowScroll / windowHeight >= 0.5) {
+    if (windowHeight <= 888) {
+      if (windowScroll / windowHeight >= 0.5) {
+        document.getElementsByClassName('mat-card')[0].classList.add('slideInAnimation');
+        document.getElementsByClassName('mat-card')[1].classList.add('slideInAnimation');
+        document.getElementsByClassName('mat-card')[2].classList.add('slideInAnimation');
+      }
+    }else if ( windowHeight > 888 ) {
       document.getElementsByClassName('mat-card')[0].classList.add('slideInAnimation');
       document.getElementsByClassName('mat-card')[1].classList.add('slideInAnimation');
       document.getElementsByClassName('mat-card')[2].classList.add('slideInAnimation');
