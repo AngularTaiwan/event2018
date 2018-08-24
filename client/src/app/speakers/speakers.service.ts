8da602e5620231fda7a1f4c9315fc5abdf38f5d3
Kevin Yang <chgc_kevin@hotmail.com>
2018-08-24 13:18:24 +0800

    start release speaker  lineup

--- client/src/app/speakers/speakers.service.ts
+++ client/src/app/speakers/speakers.service.ts
@@ -9,7 +9,7 @@ const SPEAKERS = [
     title: 'Co-Founder',
     // tslint:disable-next-line:max-line-length
     about: `Tracy is a Google Developer Expert, RxJS Core Team member, a Women Techmakers Lead, and a frequent keynote speaker at conferences. She heads up Community Relations at Node Foundation, is the host of the Modern Web podcast, organizer of This.JavaScript, Contributor Days, Google Developer Group Silicon Valley and Triangle, and RxWorkshop. She is also Co-Founder of This Dot Labs, an elite consultancy helping teams build front end applications. You can find her on Twitter @ladyleet or at http://thisdot.co/labs.`,
-    hide: false,
+    hide: true,
     fb: '',
     twitter: 'https://twitter.com/ladyleet',
     github: ''
@@ -21,7 +21,7 @@ const SPEAKERS = [
     title: '技術總監',
     // tslint:disable-next-line:max-line-length
     about: `現任多奇數位創意有限公司技術總監。連續 10 度當選微軟最有價值專家 (ASP.NET/IIS MVP)。早期專研 LAMP 相關技術，自 2005 年起轉向 Microsoft .NET Platform 開發技術，熟悉各式 Web 開發技術、網站效能調校、SQL Server、網站內容管理系統、網路系統整合。`,
-    hide: false,
+    hide: true,
     fb: 'https://www.facebook.com/will.fans/',
     twitter: 'https://twitter.com/Will_Huang',
     github: ''
@@ -32,7 +32,7 @@ const SPEAKERS = [
     company: 'Angular Taiwan ',
     title: '技術傳教士',
     about: `本次 Angular Taiwan 2018 技術大會總召，同時也是 Angular 技術專家，2018 獲選 Google Developer Expert 殊榮。`,
-    hide: false,
+    hide: true,
     fb: 'https://www.facebook.com/CKNotepad',
     twitter: 'https://twitter.com/chgc',
     github: 'http://github.com/chgc'
@@ -62,7 +62,7 @@ const SPEAKERS = [
     // tslint:disable-next-line:max-line-length
     about: `現任多奇數位創意有限公司全端工程師，2018 年以「Angular Material 完全攻略」系列文章榮獲 2018 iT 邦幫忙鐵人賽 Modern Web 組 冠軍，熟悉 .NET 與 Angular 相關技術，熱衷各式新奇技術研究與分享學習經驗。
     `,
-    hide: false,
+    hide: true,
     fb: 'https://www.facebook.com/fullstackledder/',
     twitter: '',
     github: 'https://github.com/wellwind'
@@ -82,7 +82,7 @@ const SPEAKERS = [
       <li>Global Azure Bootcamp 講師</li>
     </ul>
     `,
-    hide: false,
+    hide: true,
     fb: 'https://www.facebook.com/poychang',
     twitter: 'https://twitter.com/poychang',
     github: ''
